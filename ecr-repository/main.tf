6364079f342b86550c62b08fae19d1f30238c738
theifedayo <adesiyanifedayo@gmail.com>
2023-08-14 12:42:27 +0100

    add infra-architecture picture

--- ecr-repository/main.tf
+++ ecr-repository/main.tf
@@ -8,6 +8,6 @@ resource "aws_ecr_repository" "my_app" {
   name = "my-docker-repo"
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.my_app.repository_url
-}
+# output "ecr_repository_url" {
+#   value = aws_ecr_repository.my_app.repository_url
+# }
